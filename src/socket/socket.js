c8906a827f058f7e3b9c928d11255f4a74f2805d
CS-parth <parth.vijy@gmail.com>
2024-08-02 17:07:43 +0530

    fix: changed socker URL for

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,4 @@
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
+const URL = process.env.NODE_ENV === 'production' ? process.env.SERVER_URL : 'http://localhost:4000';
 export const  socket = io(URL,{autoConnect:false});
